4f6cdd8972aa9fffeb2aaa0e84f956e9efe5277f
BrunoBafilli <brunobafilli@gmail.com>
2023-11-24 07:23:49 -0300

    t
    t

--- cifraDeCesar/Program.cs
+++ cifraDeCesar/Program.cs
@@ -1,65 +1,22 @@
-﻿using System;
-					
-public class Program
-{
-	public static void Main()
-	{
-		string frase = "AmEIXa_abd";
+﻿string frase = "AABCCCDEEEE";
 
-char[] criptada = CifraDeCesar(frase, 1, true);
-char[] decriptada = CifraDeCesar(frase, 1, false);
 
-Console.WriteLine(criptada);
-Console.WriteLine(decriptada);
+string novafrase = "";
+int count = 1;
+for(int i = 0; i < frase.Length; i++){
 
-char[] CifraDeCesar(string msg, int chave, bool cript){
-
-    if(!cript)
-        chave -= chave;
-
-    char[] alfabeto = new char[26] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    char[] ALFABETO = new char[26] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    
-    char[] fraseCifrada = new char[msg.Length];
-
-    for(int i = 0; i < msg.Length; i++){
-
-        bool ehLetra = false;
-        bool EhCaixaAlta = false;
-        int posLetra = 0;
-
-        for(int t = 0; t < alfabeto.Length; t++){
-            if(msg[i] == ALFABETO[t]){
-                posLetra = t;
-                ehLetra = true;
-                EhCaixaAlta = true;
-                break;
-            }
-        }
-
-        if(!EhCaixaAlta){
-            for(int t = 0; t < alfabeto.Length; t++){
-                if(msg[i] == alfabeto[t]){
-                    posLetra = t;
-                    ehLetra = true;
-                    break;
-                }
-            }
-        }
-        if(ehLetra){
-            char letraCifrada;
-            if(EhCaixaAlta)
-                letraCifrada = Convert.ToChar(ALFABETO[(posLetra + chave) % alfabeto
-                .Length]);
-            else
-                letraCifrada = Convert.ToChar(alfabeto[(posLetra + chave) % alfabeto.Length]);
-
-            fraseCifrada[i] = letraCifrada;
-        } else {
-            fraseCifrada[i] += msg[i];
+    if(i < frase.Length -1 && frase[i] == frase[i+1]){
+        count++;
+    } else {
+        if(count == 1){
+            novafrase += frase[i];
+        } 
+        else {
+            novafrase += count+""+frase[i];
+            count = 1;
         }
     }
-    return fraseCifrada;
+
 }
-	}
-}
+
+System.Console.WriteLine(novafrase);
--- cifraDeCesar/obj/Debug/net8.0/cifraDeCesar.AssemblyInfo.cs
+++ cifraDeCesar/obj/Debug/net8.0/cifraDeCesar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa44c12cedd20e93b9fb807cf06b59d08d74b139")]
 [assembly: System.Reflection.AssemblyProductAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
