c5eafd4da158fdf9d43ef55508c9d95d588145c3
BrunoBafilli <brunobafilli@gmail.com>
2023-11-24 07:31:27 -0300

    t
    t

--- cifraDeCesar/Program.cs
+++ cifraDeCesar/Program.cs
@@ -1,22 +1,41 @@
-﻿string frase = "AABCCCDEEEE";
+﻿string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+string ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+string msg = "ameixa_A";
+int chave = 1;
 
-string novafrase = "";
-int count = 1;
-for(int i = 0; i < frase.Length; i++){
+string textoCifrado = cifrada(true);
+string textoPuro = cifrada(false);
 
-    if(i < frase.Length -1 && frase[i] == frase[i+1]){
-        count++;
-    } else {
-        if(count == 1){
-            novafrase += frase[i];
-        } 
-        else {
-            novafrase += count+""+frase[i];
-            count = 1;
+Console.WriteLine(textoCifrado);
+Console.WriteLine(textoPuro);
+
+string cifrada(bool cifrar){
+
+    if(!cifrar)
+        chave -= chave;
+
+    string cifrada = "";
+    for (int i = 0; i < msg.Length; i++)
+    {
+        bool f = false;
+        for (int t = 0; t < alfabeto.Length; t++)
+        {
+            if(alfabeto[t] == msg[i])
+            {
+                cifrada += alfabeto[t + chave];
+                f = true;
+                break;
+            } 
+            else if (ALFABETO[t] == msg[i])
+            {
+                cifrada += ALFABETO[t + chave];
+                f = true;
+                break;
+            }
         }
+        if (!f)
+            cifrada += msg[i];
     }
-
+    return cifrada;
 }
-
-System.Console.WriteLine(novafrase);
--- cifraDeCesar/obj/Debug/net8.0/cifraDeCesar.AssemblyInfo.cs
+++ cifraDeCesar/obj/Debug/net8.0/cifraDeCesar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4053c123383e8598765ed3b45dc5b850ea400b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cifraDeCesar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
