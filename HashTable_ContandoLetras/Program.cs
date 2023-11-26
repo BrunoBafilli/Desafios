7aad968afcb43d4170f9f2b5281f1fc6495f29e9
BrunoBafilli <brunobafilli@gmail.com>
2023-11-26 18:02:22 -0300

    t
    t

---
+++ HashTable_ContandoLetras/Program.cs
@@ -0,0 +1,23 @@
+﻿string msg = "Hello, World! 123";
+
+Dictionary<char, int> hashTable = new Dictionary<char, int>();
+
+for (int i = 0; i < msg.Length; i++)
+{
+    if (char.IsLetterOrDigit(msg[i]))
+    {
+        if (hashTable.ContainsKey(msg[i]))
+        {
+            hashTable[msg[i]] ++;
+        }
+        else
+        {
+            hashTable.Add(msg[i],1);
+        }
+    }
+}
+
+foreach (var keyValuePair in hashTable)
+{
+    Console.WriteLine($"{keyValuePair.Key} - {keyValuePair.Value}");
+}
